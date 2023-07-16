66a4f81ae2f80935cc16ef697e5e0063fe48bb12
hossainshakil242 <realshakil8@gmail.com>
2023-07-16 23:03:53 +0600

    install tailwind,react-router,using vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <p className='bg-purple-700 text-yellow-100 text-4xl text-center font-bold py-5'>Real shakil</p>
+        </div>
+    );
+};
+
+export default App;
